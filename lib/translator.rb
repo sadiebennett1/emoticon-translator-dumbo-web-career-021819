5783bf7ddf6a53f824d2d98ad7e5fbb9a49e8244
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-18 00:58:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,20 +4,20 @@ require "yaml"
 def load_library(file)
   # code goes here
   hash = YAML.load_file('./lib/emoticons.yml')
-  # hash = {angel: ["O:)", "☜(⌒▽⌒)☞"], angry: [">:(", "ヽ(ｏ`皿′ｏ)ﾉ"]
-  # getmeaning = {}
-  # getemoticon = {}
-  # new_hash = {}
-  # hash.each do |meaning, emoticons|
-  #   getmeaning[meaning] = emoticons[1]
-  #   getemoticon[emoticon[0]] = emoticon[1]
-  # end
-  # new_hash[get_meaning] = getmeaning
-  # new_hash[get_emoticon] = getemoticon
-  # # new_hash = {get_meaning=> {jap_emot => meaning, jap_emot => meaning},
-  # #             get_emoticon => {english_emot => jap_emot, english_emot => jap_emot}}
-  #
-  # return new_hash
+  hash = {angel: ["O:)", "☜(⌒▽⌒)☞"], angry: [">:(", "ヽ(ｏ`皿′ｏ)ﾉ"]
+  getmeaning = {}
+  getemoticon = {}
+  new_hash = {}
+  hash.each do |meaning, emoticons|
+    getmeaning[meaning] = emoticons[1]
+    getemoticon[emoticon[0]] = emoticon[1]
+  end
+  new_hash[get_meaning] = getmeaning
+  new_hash[get_emoticon] = getemoticon
+  # new_hash = {get_meaning=> {jap_emot => meaning, jap_emot => meaning},
+  #             get_emoticon => {english_emot => jap_emot, english_emot => jap_emot}}
+  
+  return new_hash
 end
 
 def get_japanese_emoticon
