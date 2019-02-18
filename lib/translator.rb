d6cfefb8b948d856b41d2c25590dca0e282b3991
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-18 01:15:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,7 +24,7 @@ def get_japanese_emoticon(file, emoticon)
   # code goes here
   hash = load_library(file)
   hash.each do |get, hash|
-    if get == "get_emoticon" && hash.values.include?(emoticon)
+    if get == "get_emoticon" 
       hash.each do |english, japanese|
         if english == emoticon
           return japanese
