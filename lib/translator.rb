b8ceb7e9c490cb23343d38ec3e1161666958ad16
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-18 00:28:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require "yaml"
 
 def load_library(file)
   # code goes here
-  emoticons = YAML.load_file('emoticons.yml')
+  emoticons = YAML.load_file('./lib/emoticons.yml')
 end
 
 def get_japanese_emoticon
