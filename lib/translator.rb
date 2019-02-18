69311fd0a9cc499e59e7a213a59869f46b9d5c6c
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-18 01:14:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,6 +29,7 @@ def get_japanese_emoticon(file, emoticon)
         if english == emoticon
           return japanese
         end
+      end
     else
       return "Sorry, that emoticon was not found"
     end
